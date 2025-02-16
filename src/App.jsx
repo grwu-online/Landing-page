977219bfa5f4dc21ff73402b62fe237251a29f19
sshikharr <shikhardwivedi2019@gmail.com>
2025-02-17 02:22:13 +0530

    Added static.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import './App.css';
-import { SiteHeader } from './components/site-header';
-import { HeroSection } from './components/hero-section';
-import { MissionSection } from './components/mission-section';
-import { BrandsSection } from './components/brand-section';
-import { ServicesSection } from './components/services-section';
-import WhyUsSection from './components/why-us-section';
-import { ScrollProgressBar } from './components/scroll-progress-bar';
-import { TeamSection } from './components/team-section';
-import { Footer } from './components/site-footer';
+import { SiteHeader } from './components/site-header.jsx';
+import { HeroSection } from './components/hero-section.jsx';
+import { MissionSection } from './components/mission-section.jsx';
+import { BrandsSection } from './components/brand-section.jsx';
+import { ServicesSection } from './components/services-section.jsx';
+import WhyUsSection from './components/why-us-section.jsx';
+import { ScrollProgressBar } from './components/scroll-progress-bar.jsx';
+import { TeamSection } from './components/team-section.jsx';
+import { Footer } from './components/site-footer.jsx';
 
 function App() {
 
